d13e12ab710bd65779c39bf58a952d6fad0e72a6
Liam <liam.mulv94@gmail.com>
2020-03-03 17:24:57 +1100

    testing - facebook login button callback

--- imports/ui/pages/facebook_login/FacebookLogin.js
+++ imports/ui/pages/facebook_login/FacebookLogin.js
@@ -4,16 +4,19 @@ import {FacebookLoginContainer} from './FacebookLogin.sc'
 
 function FacebookLogin() {
 
-	FB.Event.subscribe('auth.login', function(response) {
-		console.log(response)
-	  	// do something with response
-	});
+	function checkLoginState() {
+		console.log('Check login state clicked')
+	  	FB.getLoginStatus(function(response) {
+	  		console.log('Button response', response)
+	   		statusChangeCallback(response);
+	  	})
+	}
 
 	return (
 		<FacebookLoginContainer>
 			<div>
 				<h1>Login with facebook</h1>
-				<div class="fb-login-button" data-scrope="instagram_basic,pages_show_list" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+				<div class="fb-login-button" onlogin="checkLoginState();" data-scrope="instagram_basic,pages_show_list" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
 			</div>
 		</FacebookLoginContainer>
 	)
