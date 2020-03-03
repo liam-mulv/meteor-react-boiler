3d14859b65374b9a676e39d2cd3809b20cbfe3c4
Liam <liam.mulv94@gmail.com>
2020-03-03 18:13:42 +1100

    Replaced facebook login with custom NPM package

--- imports/ui/pages/facebook_login/FacebookLogin.js
+++ imports/ui/pages/facebook_login/FacebookLogin.js
@@ -1,31 +1,40 @@
 import React, {useEffect, useState} from 'react'
 
+// NPM
+import { FacebookProvider, LoginButton } from 'react-facebook';
+
+// Styled Components
 import {FacebookLoginContainer} from './FacebookLogin.sc'
+
+// Icon
+import FacebookCircleIcon from '../../icons/facebook-circle.js'
 
 function FacebookLogin() {
 
-	function checkLoginState() {
-		console.log('Check login state 1 clicked')
-	  	FB.getLoginStatus(function(response) {
-	  		console.log('Button response', response)
-	   		statusChangeCallback(response);
-	  	})
+	handleResponse = (data) => {
+	    console.log(data);
 	}
-
-	function checkLoginStateTwo() {
-		console.log('Check login state 2 clicked')
-	  	FB.getLoginStatus(function(response) {
-	  		console.log('Button response', response)
-	   		statusChangeCallback(response);
-	  	})
+	 
+	handleError = (error) => {
+		console.log(error)
+	    // this.setState({ error });
 	}
 
 	return (
 		<FacebookLoginContainer>
 			<div>
-				<h1>Login with facebook</h1>
-				<div class="fb-login-button" data-onlogin="checkLoginStateTwo();" onlogin="checkLoginState();" data-scrope="public_profile,email,instagram_basic,pages_show_list" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
-			</div>
+				<h1>Login</h1>
+				<FacebookProvider appId="509886203235331">
+			        <LoginButton
+			          scope="public_profile,email,instagram_basic,pages_show_list"
+			          onCompleted={this.handleResponse}
+			          onError={this.handleError}
+			        >
+			          <span><FacebookCircleIcon/>Continue with Facebook</span>
+			        </LoginButton>
+			    </FacebookProvider>
+{/*				<div class="fb-login-button" data-onlogin={() => checkLoginStateTwo()}  data-scrope="public_profile,email,instagram_basic,pages_show_list" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="false" data-use-continue-as="true"></div>
+*/}			</div>
 		</FacebookLoginContainer>
 	)
 }
--- imports/ui/pages/facebook_login/FacebookLogin.sc.js
+++ imports/ui/pages/facebook_login/FacebookLogin.sc.js
@@ -5,15 +5,35 @@ export const FacebookLoginContainer = styled.div`
     height: calc(100vh - 81px);
 	text-align: center;
 	align-items: center;
-	
+
+
+
 	> div {
 		flex: 1;
 	}
 
 	h1 {
-		font-size: 2rem;
+		font-size: 2.5rem;
    		margin-bottom: 45px;
 	}
 
+	button {
+	    background-color: #1877f2;
+	    color: #fff;
+	    border: 0px;
+	    border-radius: 4px;
+	    padding: 15px 20px;
+	    font-size: 1.4rem;
+	    font-weight: 700;
+	}
 
+	button span {
+		display: flex;
+    	align-items: center;
+	}
+
+	button svg {
+		width: 30px;
+    	margin-right: 10px;
+	}
 `
